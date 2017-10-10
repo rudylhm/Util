5269c5bc4fa9856416b2aee1402aae92b89a6cd0
rudyl <rudyl@RUDYLIU-PC>
2017-10-10 14:14:29 +0800

    添加了枚举类工具 添加了Redis的支持类 添加了Guid的简单处理方法

---
+++ Util/Util/Enum/EnumHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.EnumCommon
+{
+    public class Helper
+    {
+        public static string GetEnumDescription(Enum value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            string description = value.ToString();
+            System.Reflection.FieldInfo fieldinfo = value.GetType().GetField(description);
+            EnumDescriptionAttribute[] attributes = (EnumDescriptionAttribute[])fieldinfo.GetCustomAttributes(typeof(EnumDescriptionAttribute), false);
+            if (attributes != null && attributes.Length > 0)
+            {
+                description = attributes[0].Description;
+            }
+            return description;
+        }
+
+        //public static int IntToEnum()
+        //{
+
+        //}
+    }
+
+    public sealed class EnumDescriptionAttribute : Attribute
+    {
+        public string Description { get; set; }
+
+        public EnumDescriptionAttribute(string description)
+                : base()
+        {
+            this.Description = description;
+        }
+    }
+}
---
+++ Util/Util/StringCommon/GuidHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.StringCommon
+{
+    public class GuidHelper
+    {
+        public static string GetNewGuid()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+    }
+}
